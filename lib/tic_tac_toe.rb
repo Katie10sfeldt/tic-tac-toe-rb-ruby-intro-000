71e611fa5c7f1e66e867cbc30bb3cc8720924e23
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-16 20:31:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,19 +29,48 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
+def turn_count(board)
+  #must return the number of turns that have been taken
+  num_of_turns = 0
+
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      num_of_turns += 1
+    end
+  end
+  num_of_turns
+end
+
+# Determines which symbol will be printed
+def current_player(board)
+  #uses the '%' to determine whose turn it is. hint: turn count % 2 determines what is printed.
+  turn_symbol = " "
+
+  if turn_count(board) % 2 == 1
+    turn_symbol = "O"
+  elsif turn_count(board) % 2 == 0
+    turn_symbol = "X"
+  end
+
+  turn_symbol
+end
+
+# Combines helper functions and sets the process for each turn
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    move(board, index, current_player(board))
     display_board(board)
   else
     turn(board)
   end
 end
 
-# Define your play method below
+
+
+# Ensures that there will be 9 rounds (1 for each space on the board)
 def play(board)
   counter = 1
 
